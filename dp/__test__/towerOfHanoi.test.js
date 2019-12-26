25e67bae982fe8dcb21db8b23b465677e08f7610
namedboy <kkokey@gmail.com>
2019-12-26 13:19:59 +0900

    Change making test data

--- dp/__test__/towerOfHanoi.test.js
+++ dp/__test__/towerOfHanoi.test.js
@@ -2,8 +2,8 @@ import towerOfHanoi from "../towerOfHanoi";
 
 describe('', () => {
     it('should two plus two is four', () => {
-        // towerOfHanoi.moveTower('start', 'during', 'end' , 3);
+        towerOfHanoi.moveTower('start', 'during', 'end' , 3);
         towerOfHanoi.setDefault();
-        towerOfHanoi.moveSet('start', 'during', 'end');
+        // towerOfHanoi.moveSet('start', 'during', 'end');
     });
 });
--- dp/towerOfHanoi.js
+++ dp/towerOfHanoi.js
@@ -14,24 +14,25 @@ export default {
             return false;
         }
         this.moveTower(start, end, during, towerLang - 1);
-        console.log('%d번 원반을 %s에서 %s로 옮깁니다.', towerLang, start, during);
+        process.stdout.write(`${towerLang}번 원반을 ${start}에서 ${during}로 옮깁니다.\n`);
         this.moveTower(end, during, start, towerLang - 1);
     },
 
-    moveSet: function(s, d, e) {
-        console.log('%d번 원반을 %s에서 %s로 옮깁니다.', this.start, s, d);
-        this.start--;
-        this.during++;
-        this.getStatus();
-        console.log('%d번 원반을 %s에서 %s로 옮깁니다.', this.start, s, e);
-        this.start--;
-        this.end++;
-        this.getStatus();
-        console.log('%d번 원반을 %s에서 %s로 옮깁니다.', this.during, d, e);
-        this.during--;
-        this.end++;
-        this.getStatus();
-    },
+    // TODO 완성 해야 함.
+    // moveSet: function(s, d, e) {
+    //     process.stdout.write(`${this.start}번 원반을 ${s}에서 ${d}로 옮깁니다.`);
+    //     this.start--;
+    //     this.during++;
+    //     this.getStatus();
+    //     process.stdout.write(`${this.start}번 원반을 ${s}에서 ${e}로 옮깁니다.`);
+    //     this.start--;
+    //     this.end++;
+    //     this.getStatus();
+    //     process.stdout.write(`${this.during}번 원반을 ${d}에서 ${e}로 옮깁니다.`);
+    //     this.during--;
+    //     this.end++;
+    //     this.getStatus();
+    // },
 
     getStatus: function() {
         console.log('s:%d d:%d e:%d', this.start, this.during, this.end);
